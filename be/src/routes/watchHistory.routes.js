1b8f8f8ea1750a0c31d64e192de05ff7974b0c10
ManvendraPSdev <welcome2github@gmail.com>
2026-04-13 12:57:03 +0530

    Enhance watch history routes by integrating authentication and validation middleware. Update endpoints for adding, retrieving, and clearing watch history entries.

--- be/src/routes/watchHistory.routes.js
+++ be/src/routes/watchHistory.routes.js
@@ -1,7 +1,12 @@
 import express from "express" ; 
+import { addWatchHistory, clearWatchHistory, getWatchHistory } from "../controllers/watchHistory.controllers";
+import validate from "../middlewares/validate.middleware";
+import { authenticate } from "../middlewares/auth.middleware";
 
 export const watchHistoryRouter = express.Router() ; 
 
-watchHistoryRouter.post("/") ; 
-watchHistoryRouter.get("/") ; 
-watchHistoryRouter.delete("/") ; 
+watchHistoryRouter.use(authenticate) ;
+
+watchHistoryRouter.post("/" , validate , addWatchHistory) ; 
+watchHistoryRouter.get("/" , validate , getWatchHistory) ; 
+watchHistoryRouter.delete("/"  , clearWatchHistory) ; 
