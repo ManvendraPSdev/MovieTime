96de4e5168ec4ac90d534b7e73f1e90cf918c8bf
ManvendraPSdev <welcome2github@gmail.com>
2026-04-13 12:36:26 +0530

    Add watch history route to the main application. Integrate watchHistoryRouter for handling watch history API requests.

--- be/src/app.js
+++ be/src/app.js
@@ -7,6 +7,7 @@ import authRouter  from "./routes/auth.routes.js";
 import { adminRouter } from "./routes/admin.routes.js";
 import { movieRouter } from "./routes/movie.routes.js";
 import { favRouter } from "./routes/favorite.routes.js";
+import { watchHistoryRouter } from "./routes/watchHistory.routes.js";
 
 const app = express() ; 
 app.use(express.json()) ; 
@@ -16,6 +17,7 @@ app.use("/api/auth" , authRouter) ;
 app.use("/api/admin" , adminRouter) ; 
 app.use("/api/movie" , movieRouter) ; 
 app.use("/api/fav" , favRouter) ; 
+app.use("/api/watchHistory" , watchHistoryRouter) ; 
 
 app.use(errorHandeler) ; 
 
