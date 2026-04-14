8a09fb354e275508520633d268137cc2bde4f6f0
ManvendraPSdev <welcome2github@gmail.com>
2026-04-14 08:44:43 +0530

    Refactor App component to use AuthProvider and implement routing with AppRoutes for login and registration pages.

--- fe/src/App.jsx
+++ fe/src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { AuthProvider } from './features/auth/auth.context'
+import { AppRoutes } from './AppRoutes'
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <AuthProvider>
+      <AppRoutes/>
+    </AuthProvider>
   )
 }
 
---
+++ fe/src/AppRoutes.jsx
@@ -0,0 +1,14 @@
+import { BrowserRouter , Route , Routes } from "react-router";
+import Register from "./features/auth/pages/Register";
+import Login from "./features/auth/pages/Login";
+
+export function AppRoutes(){
+    return(
+        <BrowserRouter>
+        <Routes>
+            <Route path="/register" element={<Register/>} />
+            <Route path="/login" element={<Login/>} />
+        </Routes>
+        </BrowserRouter>
+    )
+}
