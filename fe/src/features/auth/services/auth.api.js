901c00aafe4a9cd1cfdb04a381f124aa16118c50
ManvendraPSdev <welcome2github@gmail.com>
2026-04-14 08:29:21 +0530

    Add authentication feature with login and registration pages. Integrate Axios for API calls to handle user authentication. Remove unused App.css file and simplify App component structure.

---
+++ fe/src/features/auth/services/auth.api.js
@@ -0,0 +1,46 @@
+import axios from "axios" ; 
+
+const api = axios.create({
+    baseURL : "http://localhost:3000/api/auth" , 
+    withCredentials : true
+})
+
+export async function register({userName , email , password}){
+    try {
+        const response = await api.post("/register" , {
+            userName , email , password
+        })
+        return response.data ; 
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export async function login({email , password}){
+    try {
+        const response = await api.post("/login" , {
+            email , password
+        })
+        return response.data ; 
+    } catch (error) {
+        console.log(error) ; 
+    }
+}
+
+export async function logout(){
+    try {
+        const response = await api.post("/logout") ; 
+        return response.data ; 
+    } catch (error) {
+        console.log(error) ; 
+    }
+}
+
+export async function getMer(){
+    try {
+        const response = await api.get("/getMe") ; 
+        return response.data ; 
+    } catch (error) {
+        console.log(error) ; 
+    }
+}
